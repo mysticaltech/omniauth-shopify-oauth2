17425a758f0e0ac371a21abbe8684aca7a7cf001
Karim Naufal <karim.naufal@me.com>
2021-03-23 22:57:12 +0100

    debugging invalid_signature

--- lib/omniauth/strategies/shopify.rb
+++ lib/omniauth/strategies/shopify.rb
@@ -11,8 +11,8 @@ module OmniAuth
       CODE_EXPIRES_AFTER = 10 * MINUTE
 
       option :client_options, {
-        :authorize_url => '/admin/oauth/authorize',
-        :token_url => '/admin/oauth/access_token'
+        authorize_url: '/admin/oauth/authorize',
+        token_url: '/admin/oauth/access_token'
       }
 
       option :callback_url
@@ -27,15 +27,15 @@ module OmniAuth
         strategy = env['omniauth.strategy']
 
         shopify_auth_params = strategy.session['shopify.omniauth_params'] ||
-          strategy.session['omniauth.params'] ||
-          strategy.request.params
+                              strategy.session['omniauth.params'] ||
+                              strategy.request.params
 
-        shopify_auth_params = shopify_auth_params && shopify_auth_params.with_indifferent_access
+        shopify_auth_params &&= shopify_auth_params.with_indifferent_access
         shop = if shopify_auth_params && shopify_auth_params['shop']
-          "https://#{shopify_auth_params['shop']}"
-        else
-          ''
-        end
+                 "https://#{shopify_auth_params['shop']}"
+               else
+                 ''
+               end
 
         strategy.options[:client_options][:site] = shop
       }
@@ -54,16 +54,28 @@ module OmniAuth
       end
 
       def valid_site?
-        !!(/\A(https|http)\:\/\/[a-zA-Z0-9][a-zA-Z0-9\-]*\.#{Regexp.quote(options[:myshopify_domain])}[\/]?\z/ =~ options[:client_options][:site])
+        !!(%r{\A(https|http)://[a-zA-Z0-9][a-zA-Z0-9\-]*\.#{Regexp.quote(options[:myshopify_domain])}/?\z} =~ options[:client_options][:site])
       end
 
       def valid_signature?
+        print "request.POST.empty? #{request.POST.empty?}\n"
         return false unless request.POST.empty?
 
         params = request.GET
         signature = params['hmac']
         timestamp = params['timestamp']
+
+        print "signature #{signature}\n"
+        print "timestamp #{timestamp}\n"
         return false unless signature && timestamp
+
+        time_test = timestamp.to_i > Time.now.to_i - CODE_EXPIRES_AFTER
+        print "timestamp.to_i > Time.now.to_i - CODE_EXPIRES_AFTER #{time_test}\n"
+        print "timestamp #{timestamp}\n"
+        print "timestamp.to_i #{timestamp.to_i}\n"
+        print "Time.now.to_i #{Time.now.to_i}\n"
+        print "Time.now #{Time.now}\n"
+        print "CODE_EXPIRES_AFTER #{CODE_EXPIRES_AFTER}\n"
 
         return false unless timestamp.to_i > Time.now.to_i - CODE_EXPIRES_AFTER
 
@@ -74,15 +86,18 @@ module OmniAuth
       end
 
       def valid_scope?(token)
-        params = options.authorize_params.merge(options_for("authorize"))
+        params = options.authorize_params.merge(options_for('authorize'))
         return false unless token && params[:scope] && token['scope']
+
         expected_scope = normalized_scopes(params[:scope]).sort
         (expected_scope == token['scope'].split(SCOPE_DELIMITER).sort)
       end
 
       def normalized_scopes(scopes)
         scope_list = scopes.to_s.split(SCOPE_DELIMITER).map(&:strip).reject(&:empty?).uniq
-        ignore_scopes = scope_list.map { |scope| scope =~ /\A(unauthenticated_)?write_(.*)\z/ && "#{$1}read_#{$2}" }.compact
+        ignore_scopes = scope_list.map do |scope|
+          scope =~ /\A(unauthenticated_)?write_(.*)\z/ && "#{Regexp.last_match(1)}read_#{Regexp.last_match(2)}"
+        end.compact
         scope_list - ignore_scopes
       end
 
@@ -94,7 +109,7 @@ module OmniAuth
       end
 
       def self.hmac_sign(encoded_params, secret)
-        OpenSSL::HMAC.hexdigest(OpenSSL::Digest::SHA256.new, secret, encoded_params)
+        OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('SHA256'), secret, encoded_params)
       end
 
       def valid_permissions?(token)
@@ -107,7 +122,7 @@ module OmniAuth
       end
 
       def fix_https
-        options[:client_options][:site] = options[:client_options][:site].gsub(/\Ahttp\:/, 'https:')
+        options[:client_options][:site] = options[:client_options][:site].gsub(/\Ahttp:/, 'https:')
       end
 
       def setup_phase
@@ -124,15 +139,24 @@ module OmniAuth
       end
 
       def callback_phase
-        return fail!(:invalid_site, CallbackError.new(:invalid_site, "OAuth endpoint is not a myshopify site.")) unless valid_site?
-        return fail!(:invalid_signature, CallbackError.new(:invalid_signature, "Signature does not match, it may have been tampered with.")) unless valid_signature?
+        unless valid_site?
+          return fail!(:invalid_site,
+                       CallbackError.new(:invalid_site, 'OAuth endpoint is not a myshopify site.'))
+        end
+        unless valid_signature?
+          return fail!(:invalid_signature,
+                       CallbackError.new(:invalid_signature,
+                                         'Signature does not match, it may have been tampered with.'))
+        end
 
         token = build_access_token
         unless valid_scope?(token)
-          return fail!(:invalid_scope, CallbackError.new(:invalid_scope, "Scope does not match, it may have been tampered with."))
+          return fail!(:invalid_scope,
+                       CallbackError.new(:invalid_scope, 'Scope does not match, it may have been tampered with.'))
         end
         unless valid_permissions?(token)
-          return fail!(:invalid_permissions, CallbackError.new(:invalid_permissions, "Requested API access mode does not match."))
+          return fail!(:invalid_permissions,
+                       CallbackError.new(:invalid_permissions, 'Requested API access mode does not match.'))
         end
 
         super
